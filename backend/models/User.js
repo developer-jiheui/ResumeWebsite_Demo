a549a6424f42541d773323748ba55f15c29af4c4
Jiheui <developer.jiheuilee@gmail.com>
2024-02-16 17:49:55 +0900

    first db schema

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  title: {
+    type: String,
+    maxlength: 15
+  },
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
